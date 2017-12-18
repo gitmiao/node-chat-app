ffbe1c83ecfe7a2ddacad7df5dcb20109f0c579e
ruichen <cindy.chen800@gmail.com>
2017-12-18 05:12:53 -0500

    fix error

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 
-var app = express();
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`server start on ${port}`);
 });
