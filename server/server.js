254045821b438d1987b8791ed6d9eecc9024e441
ruichen <cindy.chen800@gmail.com>
2017-12-18 04:05:35 -0500

    init commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+var app = express();
+const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+  console.log(`server start on ${port}`);
+});
